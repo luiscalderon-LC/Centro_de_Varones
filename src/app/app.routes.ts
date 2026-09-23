4b697099786b3029a197054dff5276a578f0db05
luiscalderon-LC <luiscalderon@vallegrande.edu.pe>
2026-09-23 10:31:01 -0500

    chore: limpiar rama main

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { Inicio } from './features/landing/pages/inicio/inicio';
-
-export const routes: Routes = [
-  {
-    path: '',
-    component: Inicio
-  }
-];
